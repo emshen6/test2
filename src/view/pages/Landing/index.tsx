186f2c914ab97c7e43814dbe6999187575f9adb4
Alan <tyujn24@mail.ru>
2024-01-20 06:32:23 +0300

    shit

--- src/view/pages/Landing/index.tsx
+++ src/view/pages/Landing/index.tsx
@@ -3,8 +3,7 @@ import classNames from 'classnames';
 import styles from './index.module.scss';
 import { FactoryImage } from '../../../assets/image/FactoryImage';
 import { Header } from '../../components/Header';
-import { Footer } from '../../components/Footer/index.tsx';
-import { Benefits } from '../../components/Benefits/index.tsx';
+import { Benefits } from '../../components/Benefits';
 import { Footer } from '../../components/Footer';
 import { About } from '../../components/About';
 import { CatalogPart } from '../../components/CatalogPart';
@@ -12,17 +11,21 @@ import { CatalogPart } from '../../components/CatalogPart';
 const cx = classNames.bind(styles);
 
 export const Landing: React.FC = () => {
-    const { data } = useGetProductsQuery();
     const titles = ['a', 'b', 'c', 'd'];
     const descriptions = ['a', 'b', 'c', 'd'];
+
     return (
         <div className={cx(styles.landing)}>
-            <Header />
-            <FactoryImage />
-            <Benefits titles={titles} descriptions={descriptions} />
-            <About />
-            <CatalogPart />
-            <Footer />
+            <div className={styles.wrapper}>
+                <div>
+                    <Header />
+                    <FactoryImage />
+                </div>
+                <Benefits titles={titles} descriptions={descriptions} />
+                <About />
+                <CatalogPart />
+                <Footer />
+            </div>
         </div>
     );
 };
