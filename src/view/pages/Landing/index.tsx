6cfcd54a2e04c5020169df0eb060feb7209ec531
Alan <tyujn24@mail.ru>
2024-01-20 03:34:48 +0300

    shit

--- src/view/pages/Landing/index.tsx
+++ src/view/pages/Landing/index.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import classNames from 'classnames';
 import styles from './index.module.scss';
-import { useGetProductsQuery } from '../../../core/api/product';
 import { FactoryImage } from '../../../assets/image/FactoryImage';
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
 import { About } from '../../components/About';
+import { CatalogPart } from '../../components/CatalogPart';
 
 const cx = classNames.bind(styles);
 
 export const Landing: React.FC = () => {
-    const { data } = useGetProductsQuery();
-
-    console.log(data);
     return (
         <div className={cx(styles.landing)}>
             <Header />
             <FactoryImage />
             <About />
+            <CatalogPart />
             <Footer />
         </div>
     );
