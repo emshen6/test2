80c2414dcfa728f16e0e7bf7eacdeabbd6636f0e
emshen6 <312637@niuitmo.ru>
2024-01-20 03:38:45 +0300

    benefits

--- src/view/pages/Landing/index.tsx
+++ src/view/pages/Landing/index.tsx
@@ -8,21 +8,22 @@ import { FactoryImage } from '../../../assets/image/FactoryImage';
 import { ContentWrapper } from '../../components/ContentWrapper';
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer/index.tsx';
+import { Benefits } from '../../components/Benefits/index.tsx';
 
 const cx = classNames.bind(styles);
 
 export const Landing: React.FC = () => {
     const { data } = useGetProductsQuery();
+    const titles = ['a', 'b', 'c', 'd'];
+    const descriptions = ['a', 'b', 'c', 'd'];
 
     console.log(data);
     return (
         <div className={cx(styles.landing)}>
-            <ContentWrapper>
-                <Header />
-                <FactoryImage />
-            </ContentWrapper>
+            <Header />
+            <FactoryImage />
             <div className={'container block'}></div>
-            <Icon icon={ICON.LOGO} width={175} height={55} color={'black'} />
+            <Benefits titles={titles} descriptions={descriptions} />
             <Footer />
         </div>
     );
